21b29912e07333a62bf22de5688675955c32a6c4
vochihieu8320 <chihieu8320@gmail.com>
2024-02-16 16:41:46 +0700

    Add fanout direct exchange

--- app/services/rabbit_sevice.rb
+++ app/services/rabbit_sevice.rb
@@ -9,8 +9,8 @@ class RabbitSevice
     connection = Bunny.new("amqps://iecdgjsz:nOouVvT4l56VKVCuWOu43NmsFc34_I3L@cougar.rmq.cloudamqp.com/iecdgjsz")
     connection.start # Start a connection with the CloudAMQP server
     channel = connection.create_channel # Declare a channel
-    queue = channel.queue("bunny_queue") # Declare a queue    
-    channel.default_exchange.publish(@message, routing_key: 'bunny_queue')
+    queue = channel.queue("test5") # Declare a queue    
+    channel.default_exchange.publish(@message, routing_key: 'test5', persistent: true)
     connection.close #
   end
 
@@ -20,5 +20,14 @@ class RabbitSevice
     channel = connection.create_channel
     exchange = channel.fanout('logs')
     exchange.publish(@message)
+  end
+
+  def direct_exchange
+    connection = Bunny.new("amqps://iecdgjsz:nOouVvT4l56VKVCuWOu43NmsFc34_I3L@cougar.rmq.cloudamqp.com/iecdgjsz")
+    connection.start # Start a connection with the CloudAMQP server
+    channel = connection.create_channel
+    exchange = channel.direct('noti')
+    exchange.publish(@message, routing_key: "sms")
+    exchange.publish(@message, routing_key: "email")
   end
 end
--- app/services/subsciber_service.rb
+++ app/services/subsciber_service.rb
@@ -5,8 +5,8 @@ class SubsciberService
     connection = Bunny.new("amqps://iecdgjsz:nOouVvT4l56VKVCuWOu43NmsFc34_I3L@cougar.rmq.cloudamqp.com/iecdgjsz")
     connection.start  # Start a connection with the CloudAMQP server
     channel = connection.create_channel # Declare a channel
-    queue = channel.queue("bunny_queue") # Declare a queue
-    
+    queue = channel.queue("test5") # Declare a queue
+    channel.prefetch(1)
     begin # Consume messages
       puts ' [*] Waiting for messages. To exit press CTRL+C'
       queue.subscribe(manual_ack: true, block: true) do |_delivery_info, _properties, body|
@@ -16,6 +16,63 @@ class SubsciberService
       end
     rescue Interrupt => _
       connection.close # Close the connection
+      exit(0)
+    end
+  end
+
+  def fanout_subscriber
+    connection = Bunny.new("amqps://iecdgjsz:nOouVvT4l56VKVCuWOu43NmsFc34_I3L@cougar.rmq.cloudamqp.com/iecdgjsz")
+    connection.start  # Start a connection with the CloudAMQP server
+    channel = connection.create_channel # Declare a channel
+    queue = channel.queue('', exclusive: true)
+    queue.bind('logs')
+    
+    puts ' [*] Waiting for logs. To exit press CTRL+C'
+
+    begin
+      queue.subscribe(block: true) do |_delivery_info, _properties, body|
+        puts " [x] #{body}"
+      end
+    rescue Interrupt => _
+      channel.close
+      connection.close
+    end
+  end
+
+  def sms_direct_subscriber
+    connection = Bunny.new("amqps://iecdgjsz:nOouVvT4l56VKVCuWOu43NmsFc34_I3L@cougar.rmq.cloudamqp.com/iecdgjsz")
+    connection.start  # Start a connection with the CloudAMQP server
+    channel = connection.create_channel # Declare a channel
+    queue = channel.queue('', exclusive: true)
+    exchange = channel.direct('noti')
+    queue.bind(exchange, routing_key: "sms")
+    puts ' [*] Waiting for logs. To exit press CTRL+C'
+    begin
+      queue.subscribe(block: true) do |delivery_info, _properties, body|
+        puts " [x] #{delivery_info.routing_key}:#{body}"
+      end
+    rescue Interrupt => _
+      channel.close
+      connection.close
+      exit(0)
+    end
+  end
+
+  def email_direct_subscriber
+    connection = Bunny.new("amqps://iecdgjsz:nOouVvT4l56VKVCuWOu43NmsFc34_I3L@cougar.rmq.cloudamqp.com/iecdgjsz")
+    connection.start  # Start a connection with the CloudAMQP server
+    channel = connection.create_channel # Declare a channel
+    exchange = channel.direct('noti') # declare the exchange
+    queue = channel.queue('', exclusive: true) # declare the queue
+    queue.bind(exchange, routing_key: "email") # binding exchange
+    puts ' [*] Waiting for logs. To exit press CTRL+C'
+    begin
+      queue.subscribe(block: true) do |delivery_info, _properties, body|
+        puts " [x] #{delivery_info.routing_key}:#{body}"
+      end
+    rescue Interrupt => _
+      channel.close
+      connection.close
       exit(0)
     end
   end
